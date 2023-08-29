9e7b046bdc05ab9c75bf1befcacfd9fdb187dbea
Vel <korzhavin.vel@yandex.ru>
2023-08-29 19:29:04 +0700

    dobavlenie roditelya

--- src/Vehicles.java
+++ src/Vehicles.java
@@ -1,3 +1,34 @@
 public class Vehicles {
+    protected String modelName;
+    protected int wheelsCount;
 
+    public Vehicles(String modelName, int wheelsCount) {
+        this.modelName = modelName;
+        this.wheelsCount = wheelsCount;
+    }
+
+    public String getModelName() {
+        return modelName;
+    }
+
+    public int getWheelsCount() {
+        return wheelsCount;
+    }
+    public void updateTyre() {
+        System.out.println("Меняем покрышку");
+    }
+    public void checkEngine() {
+        System.out.println("Проверяем двигатель");
+    }
+    public void checkTrailer() {
+        System.out.println("Проверяем прицеп");
+    }
+
+    @Override
+    public String toString() {
+        return "Vehicles{" +
+                "modelName='" + modelName + '\'' +
+                ", wheelsCount=" + wheelsCount +
+                '}';
+    }
 }
