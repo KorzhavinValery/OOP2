b1db27febc4b2cbea32de6a0c9e86781924bbd59
Vel <korzhavin.vel@yandex.ru>
2023-08-31 13:57:51 +0700

    Исправлено

--- src/ServiceStationForClients.java
+++ src/ServiceStationForClients.java
@@ -1,4 +1,5 @@
 public class ServiceStationForClients implements ServiceStation {
+    @Override
     public void check(Vehicles transport) {
         System.out.println(transport.getModelName());
         transport.service();
